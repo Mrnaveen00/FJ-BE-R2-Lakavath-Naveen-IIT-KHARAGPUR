523dced15e71c47f924eed5d52effb01947d18fc
Naveen <lakavathnaveen8374@gmail.com>
2026-02-11 18:01:01 +0530

    Add Budget model to Sequelize sync

--- Backend/Models/index.js
+++ Backend/Models/index.js
@@ -7,6 +7,7 @@ const { sequelize } = require('../database');
 const User = require('./User.model');
 const Category = require('./Category.model');
 const Transaction = require('./Transaction.model');
+const Budget = require('./Budget.model');
 
 // Define associations
 User.hasMany(Category, { foreignKey: 'user_id' });
@@ -18,11 +19,18 @@ Transaction.belongsTo(User, { foreignKey: 'user_id' });
 Category.hasMany(Transaction, { foreignKey: 'category_id' });
 Transaction.belongsTo(Category, { foreignKey: 'category_id' });
 
+User.hasMany(Budget, { foreignKey: 'user_id' });
+Budget.belongsTo(User, { foreignKey: 'user_id' });
+
+Category.hasMany(Budget, { foreignKey: 'category_id' });
+Budget.belongsTo(Category, { foreignKey: 'category_id' });
+
 // Export all models
 const models = {
   User,
   Category,
   Transaction,
+  Budget,
 };
 
 // ==========================================
